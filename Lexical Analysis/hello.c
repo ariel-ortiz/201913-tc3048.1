5cc6d6d624a6e4845bc48d950bd19b65d15ece31
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-08-26 17:39:24 -0500

    Simple example using the C# regular expression API

---
+++ Lexical Analysis/hello.c
@@ -0,0 +1,13 @@
+/*
+    This is a comment!
+*/
+
+#include <stdio.h>
+
+/* Start of program. */
+int main(void) {
+    printf("Hello, World!\n");
+    return 0; /* Exit with code 0. */
+}
+/* End of program. */
+
