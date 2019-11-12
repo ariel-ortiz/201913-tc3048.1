948c18115b1c69f7b3a9beb6072143f9cc7a4cf7
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-11-11 19:09:53 -0600

    Reverse engineering C# code

---
+++ Code Generation/pow.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Pow {
+    public static void Main() {
+        int _base = 2;
+        int _exponent = 3;
+        int _result = (int) Math.Pow(_base, _exponent);
+        Console.WriteLine(_result);
+    }
+}
